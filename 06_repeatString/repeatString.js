865913ab2271f74fcf9d286b4cf7e73caba4002c
Deepta <deepta370@gmail.com>
2025-05-14 19:24:17 +0600

    completed Exercise 06 - repeatString successfully

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(name, num) {
+
+    let result="";
+    if(num<0){
+
+        return "ERROR";
+    }
+
+    for(let i=0; i<num; i++)
+    {
+
+        result+=name;
+    }
+
+    return result;
+
+
 
 };
 
