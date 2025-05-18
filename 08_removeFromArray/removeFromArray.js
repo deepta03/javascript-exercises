b30f3fca8bbf0a53d1c440f4ac98772289772b86
Deepta <deepta370@gmail.com>
2025-05-19 02:49:46 +0600

    successfully completed xercise 08 - removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...moreArgs) {
+
+    
+    
+
+    for(let i=0; i<arr.length; i++){
+
+        for(let j=0; j<moreArgs.length; j++)
+        {
+
+            if(arr[i]===moreArgs[j])
+            {
+
+                arr.splice(i, 1);
+                i=0;
+            }
+
+
+        }
+
+        
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
