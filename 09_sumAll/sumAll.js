e349bb256d3ab10d9b6126a3b3216360bd79c664
Deepta <deepta370@gmail.com>
2025-05-19 03:28:45 +0600

    completed Exercise 09 - sumAll successfully

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && num1>=0 && Number.isInteger(num2) && num2>=0){
+
+        if(num1>num2) {
+
+        [num1, num2]=[num2, num1];
+        
+        }
+
+        let total=0;
+
+        for (let i=num1; i<=num2; i++){
+
+
+            total+=i;
+        }
+
+        return total;
+
+    }
+
+
+    else {
+
+        return "ERROR";
+    }
 
 };
 
