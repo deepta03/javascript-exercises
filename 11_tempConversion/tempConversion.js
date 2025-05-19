f83c92b0fafe92ef8534bf3aaefcd6c004780d3a
Deepta <deepta370@gmail.com>
2025-05-19 17:12:37 +0600

    successfully completed Exercise 11 - tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let result=parseFloat(((temp-32)*(5/9)).toFixed(1));
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let result= parseFloat((temp*(9/5)+32).toFixed(1));
+
+  return result;
 };
 
 // Do not edit below this line
