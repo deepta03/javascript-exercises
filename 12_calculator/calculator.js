0779a4dad13d5ccbd81b00a713d1c5c445de6c6b
Deepta <deepta370@gmail.com>
2025-06-06 20:23:15 +0600

    successfully completed Exercise 12 - Calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,51 @@
-const add = function() {
+const add = function(num1, num2) {
+  
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+
+  return num1-num2;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+
+  return arr.reduce((sum, item) => sum+item, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+
+  return arr.reduce((mul, item) => mul*item, 1)
 
 };
 
-const power = function() {
+const power = function(a,b) {
+
+  return a**b;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  let result=1; 
+  
+  if(num==0) return result;
+  else if(num==1) return result;
+  else{
+   
+    for (let i=2; i<=num; i++){
+      result*=i; 
+    }
+   
+  }
+
+  return result;
 	
 };
-
+ 
 // Do not edit below this line
 module.exports = {
   add,
