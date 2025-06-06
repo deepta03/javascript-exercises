218468f617e7bc91ef8dc09d3ab32d870e5430f9
Deepta <deepta370@gmail.com>
2025-06-06 22:53:19 +0600

    completed Exercise 13 - Palindromes successfully

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+
+    let strToBeChecked=str.replace(/[^a-z0-9]/gi, '').toLowerCase().split('');
+    let strToBeCheckedReversed=strToBeChecked.slice().reverse();
+
+    for(let i=0; i<strToBeChecked.length; i++){
+
+        if(strToBeChecked[i]!=strToBeCheckedReversed[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+      
 
 };
 
